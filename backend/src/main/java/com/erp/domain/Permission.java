7e303bc660744aa833647e37c2f41045c07b5a6e
AxelAV95 <villalobos.axel@yahoo.es>
2025-10-23 15:20:38 -0600

    feat: Crear la entidad Domain/Permission [TG-65 #closed]

---
+++ backend/src/main/java/com/erp/domain/Permission.java
@@ -0,0 +1,26 @@
+package com.erp.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "permissions")
+public class Permission {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+    
+}
---
+++ backend/src/main/java/com/erp/domain/enums/RoleEnum.java
@@ -0,0 +1,5 @@
+package com.erp.domain.enums;
+public enum RoleEnum {
+    ADMIN,
+    EMPLOYEE
+}
