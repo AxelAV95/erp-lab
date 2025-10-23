36bdc3e8c303f6bc2e4682c2bb7e0bd1550b5e25
AxelAV95 <villalobos.axel@yahoo.es>
2025-10-23 17:26:36 -0600

    feat: Configurar lectura de SECRET KEY [TG-72 #closed]

--- backend/src/main/java/com/erp/infrastructure/security/JwtService.java
+++ backend/src/main/java/com/erp/infrastructure/security/JwtService.java
@@ -1,8 +1,13 @@
 package com.erp.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    @Value("${jwt.secret}") //Se hace lectura desde el application.properties
+    private String SECRET_KEY;
+
+
     
 }
